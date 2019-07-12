a4af383d6071f32311bf96faa9f95285bb43687e
Krissy Hales <kristinakfriel@gmail.com>
2019-07-12 10:50:54 -0600

    changes

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const ctrl = require('./controller');
+
+app.use(express.json());
+
+const {SERVER_PORT,CONNECTION_STRING} = process.env
+
+massive(CONNECTION_STRING)
+.then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => {
+        console.log(`Listening on port ${SERVER_PORT}`);
+    });  
+})
+.catch(error => console.log(error))
+
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header-container'>
+            <Header className='header-text'>Houser</Header>
+            </div>
+        )
+    }
+}
+
+export default Header; 
